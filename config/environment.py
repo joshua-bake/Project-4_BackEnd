8c04522033974c4f0b4c726b59c9b766b479bce5
Joshua Baker <joshua.jbaker12@icloud.com>
2024-04-19 05:26:19 -0400

    Ready for deployment

--- config/environment.py
+++ config/environment.py
@@ -1,2 +1,7 @@
-db_URI = "postgresql://localhost:5432/flashcard_db"
-SECRET = "northstoppaintzebra"
+import os
+
+db_URI = os.getenv("DATABASE_URL", "postgresql://localhost:5432/flashcard_db")
+SECRET = os.getenv("SECRET", "northstoppaintzebra")
+
+if db_URI.startswith("postgres://"):
+    db_URI = db_URI.replace("postgres://", "postgresql://", 1)
